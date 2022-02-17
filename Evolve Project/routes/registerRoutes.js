4dff3a396bc076daaf2827132474ac3eaaa9ac33
Selvathinkjs <99661184+Selvathinkjs@users.noreply.github.com>
2022-02-17 14:36:29 +0530

    Update registerRoutes.js

--- Evolve Project/routes/registerRoutes.js
+++ Evolve Project/routes/registerRoutes.js
@@ -1,6 +1,6 @@
 
 const express = require("express");
-const {  createRegister } = require("../controllers/RegisterController");
+const {  createRegister } = require(""../controllers/registerController"");
 
 const router = express.Router();
 
